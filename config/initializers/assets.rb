2d696badd502a85442f318aa49f520a4cb0333c2
peterfei <peterfeispace@gmail.com>
2017-01-19 17:20:40 +0800

    优化登陆

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
-Rails.application.config.assets.precompile += %w( admin-lte/dist/img/user2-160x160.jpg )
+Rails.application.config.assets.precompile += %w( admin-lte/dist/img/user2-160x160.jpg admin-lte/dist/img/boxed-bg.jpg admin-lte/dist/img/user4-128x128.jpg)

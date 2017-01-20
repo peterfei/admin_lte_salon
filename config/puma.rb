fa933ad023a3a23ef4e6606b7f5a96f76f729e0b
peterfei <peterfeispace@gmail.com>
2017-01-20 09:31:35 +0800

    Fixed

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,29 @@
 #!/usr/bin/env puma
 
-environment ENV['RAILS_ENV'] || 'development'
+#rails的运行环境
+# environment 'production'
+environment 'development'
+threads 2, 64
+workers 4
 
+#项目名
+app_name = "salon"
+#项目路径
+application_path = "/home/deploy/#{app_name}"
+#这里一定要配置为项目路径下地current
+directory "#{application_path}/current"
+
+#下面都是 puma的配置项
+pidfile "#{application_path}/shared/tmp/pids/puma.pid"
+state_path "#{application_path}/shared/tmp/sockets/puma.state"
+stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
+bind "unix:///#{application_path}/shared/tmp/sockets/#{app_name}.sock"
+
+#后台运行
 daemonize true
+on_restart do
+  puts 'On restart...'
+end
+preload_app!
 
-pidfile "//home/deploy/current/tmp/pids/puma.pid"
-stdout_redirect "//home/deploy/current/log/stdout", "//home/deploy/current/log/stderr"
-
-threads 0, 16
-
-bind "unix:///tmp/salon.sock"
+# unix:///home/deploy/salon/shared/tmp/sockets/salon.sock

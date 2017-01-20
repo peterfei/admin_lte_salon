10fb6102fb9b07b06624444f75e89f5e49c35db3
peterfei <peterfeispace@gmail.com>
2017-01-20 16:00:51 +0800

    Fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.serve_static_assets = false
 
 end

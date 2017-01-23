5f71f6da4aac215cd1ae84742a38c43b13fa9475
peterfei <peterfeispace@gmail.com>
2017-01-23 15:31:54 +0800

    hard restart

--- config/puma.rb
+++ config/puma.rb
@@ -20,9 +20,6 @@ state_path "#{application_path}/shared/tmp/sockets/salon.state"
 stdout_redirect "#{application_path}/shared/log/puma.stdout.log", "#{application_path}/shared/log/puma.stderr.log"
 bind "unix://#{application_path}/shared/tmp/sockets/#{app_name}.sock"
 
-
-
-
 #后台运行
 daemonize true
 on_restart do
